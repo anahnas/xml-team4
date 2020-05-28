a0caef1f8df5d0fe274a9142390f36a79ab841d3
anahnas <tomic.ana36@gmail.com>
2020-05-29 01:26:00 +0200

    codebook and user microservices added

---
+++ codebook-service/src/test/java/xmlteam4/codebookservice/CodebookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xmlteam4.codebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
