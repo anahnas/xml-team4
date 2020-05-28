5fb6dec46c41c7221c9ab2dfcfe14384b49df016
anahnas <tomic.ana36@gmail.com>
2020-05-29 01:30:27 +0200

    rent microservice added

---
+++ rent-service/src/main/java/xmlteam4/rentservice/RentServiceApplication.java
@@ -0,0 +1,15 @@
+package xmlteam4.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class RentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentServiceApplication.class, args);
+    }
+
+}
---
+++ rent-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into rent_request(start_date, end_date, client_id, status) values ('2020-05-05 19:58:00.000000', '2020-06-10 21:58:00.000000', '2', 'PAID');
+insert into rent_request(start_date, end_date, client_id, status) values ('2020-05-05 17:58:00.000000', '2020-06-10 18:58:00.000000', '2', 'PAID');
+insert into rent_request(start_date, end_date, client_id, status) values ('2020-02-05 07:58:00.000000', '2020-09-10 17:58:00.000000', '1', 'RESERVED');
